56b923d660a41afce17de523a290513e217c1f70
Paul Kinlan <paulkinlan@google.com>
2012-08-03 10:58:11 +0100

    Fixing Window opening issue

--- app/javascripts/background.js
+++ app/javascripts/background.js
@@ -15,5 +15,5 @@ chrome.experimental.app.onLaunched.addListener(function() {
     left: 100,
     top: 100
   };
-  chrome.appWindow.create('main.html', opts);
+  chrome.app.window.create('main.html', opts);
 });
